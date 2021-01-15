629dd11f27baed9ffcff39d9583e8d684d269b0e
dlinardi <dlinardi98@gmail.com>
2021-01-15 14:47:55 -0500

    implemented users/books/clubs routing for api

--- express_backend/app.js
+++ express_backend/app.js
@@ -8,6 +8,8 @@ const dbHelpers = require('./models')(db);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const clubsRouter = require('./routes/clubs');
 
 const app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter(dbHelpers));
+app.use('/api/books', booksRouter(dbHelpers));
+app.use('/api/clubs', clubsRouter(dbHelpers));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
