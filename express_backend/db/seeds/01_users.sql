d859ebfebf1933f161889d74c317a5d062df1749
dimontejulia <dimontejulia@gmail.com>
2021-01-15 14:48:04 -0500

    fixed schema

--- express_backend/db/seeds/01_users.sql
+++ express_backend/db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-  users(first_name, last_name, email, password)
+  users(id, first_name, last_name, email, password, age, gender, created_at)
 VALUES
   ('Mario', 'Bros', 'mario@nintendo.com', 'test'),
   ('Luigi', 'Bros', 'luigi@nintendo.com', 'test'),
