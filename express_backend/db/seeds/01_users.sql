f07cf5d5a101fc6e986e9d20661aba90d5538796
dimontejulia <dimontejulia@gmail.com>
2021-01-15 14:55:58 -0500

    updated user seeds

--- express_backend/db/seeds/01_users.sql
+++ express_backend/db/seeds/01_users.sql
@@ -1,18 +1,13 @@
 INSERT INTO
   users(id, first_name, last_name, email, password, age, gender, created_at)
 VALUES
-  ('Mario', 'Bros', 'mario@nintendo.com', 'test'),
-  ('Luigi', 'Bros', 'luigi@nintendo.com', 'test'),
-  (
-    'Princess',
-    'Peach',
-    'peach@nintendo.com',
-    'test'
-  ),
-  (
-    'Princess',
-    'Daisy',
-    'daisy@nintendo.com',
-    'test'
-  ),
-  ('Donkey', 'Kong', 'donkey@nintendo.com', 'test');
+(1, 'Mark', 'Twain', 'mark@books.com', 'password123', 76, 'M', '2020-01-02 10:19:15'),
+(2, 'Randi', 'Buzza', 'rbuzza1@hibu.com', 'password123', 23, 'M', '2020-03-29 18:35:14'),
+(3, 'Liuka', 'Bortolotti', 'lbortolotti2@stumbleupon.com', 'password123', 36, 'F', '2019-10-29 22:17:51'),
+(4, 'Kennett', 'Goldster', 'kgoldster3@xinhuanet.com', 'password123', 43, 'M', '2021-01-11 11:20:37'),
+(5, 'Addie', 'Creebo', 'acreebo4@tumblr.com', 'password123', 25, 'M', '2019-09-18 18:26:34'),
+(6, 'Arleyne', 'Dumini', 'adumini5@berkeley.edu', 'password123', 48, 'F', '2020-05-29 05:17:49'),
+(7, 'Gerardo', 'Shevlin', 'gshevlin6@seattletimes.com', 'password123', 35, 'M', '2020-08-15 09:36:03'),
+(8, 'Ignacio', 'Goldin', 'igoldin7@oakley.com', 'password123', 29, 'M', '2019-11-25 01:43:54'),
+(9, 'Emilia', 'O''Shevlan', 'eoshevlan8@reference.com', 'password123', 30, 'F', '2020-05-02 04:29:06'),
+(10, 'Kay', 'Skittles', 'kskittles9@illinois.edu', 'password123', 61, 'F', '2020-10-15 08:46:28');
