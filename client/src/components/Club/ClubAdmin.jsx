9f4068ba64f146ff4be90c3d3b2d64765bfeec40
dlinardi <dlinardi98@gmail.com>
2021-01-16 15:28:06 -0500

    clubAdmin basic features

--- client/src/components/Club/ClubAdmin.jsx
+++ client/src/components/Club/ClubAdmin.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import classnames from 'classnames';
+import ClubInfo from './ClubInfo';
+import Button from '../Button';
 
 export default function ClubAdmin() {
-  return <h1>ClubAdmin</h1>;
+  return (
+    <div>
+      {/* 
+          admin buttons and features will probably be where normal user
+          actions for the club are, we can switch later and refactor components as we go  
+        */}
+      <section className="book-club__header-admin">
+        <Button>Change Book</Button>
+        <Button>Schedule a Meeting</Button>
+        <br/>
+        <span>Announce Message:</span>
+      </section>
+      <section>
+        <ClubInfo />
+      </section>
+    </div>
+  );
 }
