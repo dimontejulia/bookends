f9e6307c91b9c55397384110033a56ff37fa0f9d
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-16 13:05:23 -0500

    Add child components to Book Index

--- client/src/components/Book/Index.jsx
+++ client/src/components/Book/Index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Details from './Details';
+import Rating from './Rating';
+import UserNotes from './UserNotes';
+import AlsoReadList from './AlsoReadList';
 
 export default function Index() {
-  return <h1>Book</h1>;
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <Details />
+      <Rating />
+      <UserNotes />
+      <AlsoReadList />
+    </div>
+  );
 }
