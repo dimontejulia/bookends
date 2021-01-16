ce209c3782a01f7ad7566bd80ccf25611d874005
dlinardi <dlinardi98@gmail.com>
2021-01-16 09:56:50 -0500

    books routing needs dataHelpers

--- express_backend/routes/users.js
+++ express_backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getPostsByUsers, getPostsByUser} = require('../helpers/dataHelpers');
+const {getPostsByUsers} = require('../helpers/dataHelpers');
 
 module.exports = ({ getUsers, findUserByEmail, addUser,
   getUsersPosts, getOneUsersPosts }) => {
