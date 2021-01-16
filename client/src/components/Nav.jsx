8ebdfe952cad464aab775a3146c9724a77b2ff4a
dimontejulia <dimontejulia@gmail.com>
2021-01-16 14:05:43 -0500

    navbar component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
 
 export default function Nav() {
   return (
     <div>
-      <h1>NavBar | Title| Buttons?</h1>
-      <Link to='/'>Home</Link>
-      <Link to='/shelf'>My Shelf</Link>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Logo />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <Link className="naveBarLink" to="/">
+                Home <span class="sr-only">(current)</span>{" "}
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link className="naveBarLink" to="/shelf">
+                {" "}
+                My Shelf{" "}
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link className="naveBarLink" to="/social">
+                {" "}
+                Social{" "}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
