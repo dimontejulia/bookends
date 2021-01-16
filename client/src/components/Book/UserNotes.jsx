b7b31463a72c507dfd043d36e8bd8a16048d207f
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-16 13:30:12 -0500

    Add Read Status and Date to user Notes

--- client/src/components/Book/UserNotes.jsx
+++ client/src/components/Book/UserNotes.jsx
@@ -5,7 +5,23 @@ import Rating from './Rating';
 export default function UserNotes() {
   return (
     <div>
-      <Button>Send Book to Friend</Button>
+      <div>
+        <p>Status: (ref user read date/ reading / Add to List...etc)</p>
+        <span>
+          <Button>Add to list</Button>
+          <Button>Reading</Button>
+          <label htmlFor='read'>Read: </label>
+          <input
+            type='date'
+            id='read'
+            name='read-date'
+            value='2021-01-01'
+            max='2021-04-01'
+          />
+          <Button>Edit Date</Button>
+          <Button>Send Book to Friend</Button>
+        </span>
+      </div>
       <h3>Notes</h3>
       <br />
       <textarea
