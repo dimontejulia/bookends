d975f50e8d4738a9ce43229afd7424e7b5713078
dlinardi <dlinardi98@gmail.com>
2021-01-18 11:43:48 -0500

    added carousel component/stylesheet

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Carousel.scss';
+
+export default function Carousel() {
+  
+  const props = {
+    title: "Trending Now",
+    books: [
+      {
+        name: "Atomic Habits",
+        cover_path: "https://dynamic.indigoimages.ca/books/0735211299.jpg?scaleup=true&width=614&maxheight=614&quality=85&lang=en"
+      },
+      {
+        name: "Harry Potter",
+        cover_path: "https://dynamic.indigoimages.ca/books/1408855658.jpg?scaleup=true&width=614&maxheight=614&quality=85&lang=en"
+      }
+    ]
+  };
+
+  return(
+    <section className='carousel'>
+      <h2>{props.title}</h2>
+      <div className="carousel__covers">
+        {props.books.map((book) => (
+          <img
+            className="carousel__cover"
+            src={`${props.book.cover_path}`}
+            alt={props.book.name}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
