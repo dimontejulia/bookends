6c500e0140db1f3a6e61f6d8eea441a5e7c1d973
dimontejulia <dimontejulia@gmail.com>
2021-01-19 19:15:04 -0500

    rendering book club details to clubs page

--- client/src/components/Club/ClubInfo.jsx
+++ client/src/components/Club/ClubInfo.jsx
@@ -1,36 +1,51 @@
-import React from 'react';
-import classnames from 'classnames';
-import Button from '../Button';
-import Details from '../Book/Details';
-import ClubRegular from './ClubRegular';
-import ClubAdmin from './ClubAdmin';
+import React from "react";
+import classnames from "classnames";
+import Button from "../Button";
+import Details from "../Book/Details";
+import ClubRegular from "./ClubRegular";
+import ClubAdmin from "./ClubAdmin";
 
 export default function ClubInfo(props) {
   const placeholderBook = {
-    id: 'ID',
-    title: 'Not Dynamic Prop yet',
-    author: 'That Author',
-    published: '1999',
-    description: 'A book',
+    id: "ID",
+    title: "Not Dynamic Prop yet",
+    author: "That Author",
+    published: "1999",
+    description: "A book",
     subjects: null,
     works: null,
     coverLink: `https://covers.openlibrary.org/b/olid/OL365902M-L.jpg`,
   };
+
+  /*
+  {
+    "id": 1,
+    "current_book": "OL7353617M",
+    "admin_id": 1,
+    "book_club_name": "Lighthouse Learners",
+    "date_read": "2020-07-20T04:00:00.000Z",
+    "rating": 4,
+    "comments": "A fun read with friends!",
+    "status": "finished",
+    "avatar": "https://picsum.photos/200"
+}
+  */
+
   return (
     <div>
-      <section className='book-club__header'>
+      <section className="book-club__header">
         <img
-          className='book__cover-img'
+          className="book__cover-img"
           src={props.club.avatar}
-          alt={props.club.name}
-          width='20%'
+          alt={props.club.book_club_name}
+          width="20%"
         />
         <br />
-        <h1>{props.club.name}</h1>
-        <h3>{props.club.description}</h3>
+        <h1>{props.club.book_club_name}</h1>
+        <h3>{props.club.comments}</h3>
         {props.clubAdmin ? <ClubRegular /> : <ClubAdmin />}
       </section>
-      <section className='book-club__content'>
+      <section className="book-club__content">
         <h3>Current Book:</h3>
         <Details book={placeholderBook} />
       </section>
