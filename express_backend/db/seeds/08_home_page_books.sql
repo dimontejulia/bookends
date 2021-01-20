a90b51361821da138a79d95a8a5cd9b346b124df
dimontejulia <dimontejulia@gmail.com>
2021-01-19 21:33:14 -0500

    new table and seeds files

---
+++ express_backend/db/seeds/08_home_page_books.sql
@@ -0,0 +1,10 @@
+insert into home_page_books (book_id, category)
+values
+  ('OL9039929W', 'awardWinning'),
+  ('OL16165161M','awardWinning'),
+  ('OL267933W','awardWinning'),
+  ('OL20670575W','awardWinning'),
+  ('OL27448W','awardWinning'),
+  ('OL6492763M', 'awardWinning'),
+  ('OL3140834W', 'awardWinning'),
+  ('OL15035771W', 'awardWinning')
