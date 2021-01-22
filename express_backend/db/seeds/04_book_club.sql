19fa472ada98281ae88c1f61f6a0eb373e61740b
dlinardi <dlinardi98@gmail.com>
2021-01-22 15:29:18 -0500

    updated book_club seed with new columns

--- express_backend/db/seeds/04_book_club.sql
+++ express_backend/db/seeds/04_book_club.sql
@@ -1,5 +1,4 @@
- insert into book_club (current_book, admin_id, book_club_name, date_read, rating, comments, status, avatar) 
+ insert into book_club (current_book, admin_id, book_club_name, club_description,  avatar) 
  values
-  ('OL16165161M', 1, 'Lighthouse Learners', '2020-07-20', 4,'A fun read with friends!', 'in_progress', 'https://i.pinimg.com/originals/15/fb/10/15fb10a831772d082c8a344b03bd75e8.jpg'),
-  ('OL27579080M', 2, 'Work hard, Read harder', '2019-07-20', 4,'Work friends book club!','new', 'https://i.pinimg.com/originals/34/29/17/342917a289163ce0ed9f5dc78a64e8eb.png')
-
+  ('OL16165161M', 1, 'Lighthouse Learners', 'A fun read with friends!', 'https://i.pinimg.com/originals/15/fb/10/15fb10a831772d082c8a344b03bd75e8.jpg'),
+  ('OL27579080M', 2, 'Work hard, Read harder', 'Work friends book club!', 'https://i.pinimg.com/originals/34/29/17/342917a289163ce0ed9f5dc78a64e8eb.png')
