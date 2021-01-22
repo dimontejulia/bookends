4b17f00c6118acf5bd6eebfcf2abc9af077dba37
dlinardi <dlinardi98@gmail.com>
2021-01-21 22:04:26 -0500

    created AddClub component

---
+++ client/src/components/Social/AddClub.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+export default function AddClub(props) {
+  const [form, setForm] = useState([]);
+  // const { addFriend } = props;
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setForm((prevState) => ({
+      value,
+    }));
+  };
+
+  const handleClick = (e) => {
+    console.log("EVENT", form.value);
+    props.addClub(form.value);
+    //Form resets w/o message and regardless of success or error...
+    setForm((prev) => ({ ...prev, value: "" }));
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="formClubName">
+        <Form.Control
+          onChange={handleChange}
+          value={form.value}
+          type="text"
+          placeholder="Club Name"
+        />
+      </Form.Group>
+      <Form.Group controlId="formClubAvatar">
+        <Form.Control
+          onChange={handleChange}
+          value={form.value}
+          type="url"
+          placeholder="Club Avatar Link"
+        />
+        <Form.Text className="text-muted">
+          Please use a direct link to the image!
+        </Form.Text>
+      </Form.Group>
+      <Button
+        variant="outline-primary"
+        type="submit"
+        onClick={() => handleClick()}
+      >
+        Add Club
+      </Button>
+    </Form>
+  );
+}
