bb2be64e59d6b4610d1c6cb60b1f711966f28264
dimontejulia <dimontejulia@gmail.com>
2021-01-22 17:26:35 -0500

    sizing

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 //<img src={process.env.PUBLIC_URL + "/yourPathHere.jpg"} />;
 export default function Logo() {
-  return <img src={process.env.PUBLIC_URL + "/images/BookEnds.png"} />;
+  return (
+    <img
+      style={{ width: "40%" }}
+      src={process.env.PUBLIC_URL + "/images/BookEnds.png"}
+    />
+  );
 }
