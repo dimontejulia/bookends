a605d84ee361a4af94790d4f7b1189075ed57536
dimontejulia <dimontejulia@gmail.com>
2021-01-22 13:43:17 -0500

    spinner to details page

--- client/src/components/Book/AlsoReadList.jsx
+++ client/src/components/Book/AlsoReadList.jsx
@@ -1,14 +1,14 @@
 // Friends who also read the book
 
-import React from 'react';
-import FriendsWhoReadList from '../List';
+import React from "react";
+import FriendsWhoReadList from "../List";
 
 export default function AlsoReadList(props) {
   return (
     <div>
       <FriendsWhoReadList
         list={props.friendsWhoRead}
-        listName='Friends who also read this book...'
+        listName="Friends who also read this book..."
       />
     </div>
   );
