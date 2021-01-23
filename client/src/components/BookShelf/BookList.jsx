016055aa5652973f6c41ecbc5113932f6c73f325
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-23 18:22:13 -0500

    Shelf Live search implemented

--- client/src/components/BookShelf/BookList.jsx
+++ client/src/components/BookShelf/BookList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import BookListItem from './BookListItem';
 import CardDeck from 'react-bootstrap/CardDeck';
 import FormControl from 'react-bootstrap/FormControl';
@@ -9,100 +9,67 @@ import '../Shelf.scss';
 
 export default function BookList(props) {
   const { books, setUserBooks, setCurrBook } = props;
-  const [form, setForm] = useState([]);
-  const [results, setResults] = useState(books);
-  const [searchWord, setSearchWord] = useState('');
-
-  console.log('Form', form);
-  console.log('Word', props);
+  const [results, setResults] = useState();
+  const [form, setForm] = useState('');
 
   const formatStatus = (inputStatus) => {
     switch (inputStatus) {
       case 'in_progress':
         return 'Reading';
-
       case 'finished':
         return 'Read it';
-        break;
     }
   };
 
   const handleChange = (e) => {
     const { value } = e.target;
+    console.log('FORM VALUE', e);
     setForm(value);
   };
 
-  const handleClick = (e) => {
-    setSearchWord(form.value);
-    //Form resets w/o message and regardless of success or error...
-    setForm((prev) => ({ ...prev, value: '' }));
-  };
-  const clickReset = (e) => {
-    setSearchWord('');
-    setForm((prev) => ({ ...prev, value: '' }));
-  };
-
-  // const booklistitem = function (book) {
-  //   let subject = book.subject;
-
-  //   if (Array.isArray(book.subject)) {
-  //     subject = book.subject.join();
-  //   }
-  //   if (
-  //     subject.toLowerCase().includes(searchWord.toLowerCase()) ||
-  //     book.title.toLowerCase().includes(searchWord.toLowerCase()) ||
-  //     book.author.toLowerCase().includes(searchWord.toLowerCase())
-  //   ) {
-  //     return (
-  //       <BookListItem
-  //         title={book.title}
-  //         author={book.author}
-  //         first_publish_year={book.first_publish_year}
-  //         subject={book.subject}
-  //         bookID={book.id}
-  //         bookStatus={formatStatus(book.status)}
-  //         setCurrBook={setCurrBook}
-  //       />
-  //     );
-  //   }
-  // };
-
   //map- take in obj of objs (books)
-  const searchBooks = useCallback(
-    (bookObj, searchTerm) => {
-      if (Array.isArray(searchTerm)) {
-        searchTerm = searchTerm.join();
-      }
-      searchTerm = searchTerm.toLowerCase();
-      let results = {};
-      //iterate over books obj - values
-      Object.values(bookObj).map((iBook) => {
-        //Iterate over values of inner obj
-        Object.values(iBook).map((val) => {
-          val = val.toString().toLowerCase();
+  const searchBooks = (bookObj, searchTerm) => {
+    console.log('SEARCH', bookObj, searchTerm);
+    if (Array.isArray(searchTerm)) {
+      searchTerm = searchTerm.join();
+    }
+    searchTerm = searchTerm.toLowerCase();
+    let results = {};
+    //iterate over books obj - values
+    Object.values(bookObj).map((iBook) => {
+      //Iterate over values of inner obj
+      Object.values(iBook).map((val) => {
+        val = val.toString().toLowerCase();
 
-          //Check values for search term (includes
-          if (val.includes(searchTerm)) {
-            results = { ...results, [iBook.id]: { ...iBook } };
-          }
-        });
+        //Check values for search term (includes
+        if (val.includes(searchTerm)) {
+          results = { ...results, [iBook.id]: { ...iBook } };
+        }
       });
+    });
+    //Result is object of objects send to parser
+    return results;
+  };
 
-      console.log('Restults', results);
-      //Result is object of objects send to parser
-      return results;
-    },
-    [form]
-  );
-  let searchResults = {};
   useEffect(() => {
-    //Watching for changes in form
-    //Run Search of myBooks Generate new List
+    console.log('STATE', books, results, form);
     setResults(searchBooks(books, form));
   }, [form]);
-  console.log('RES', results);
+
+  const initList = Object.values(books).map((book) => (
+    <BookListItem
+      title={book.title}
+      author={book.author}
+      first_publish_year={book.first_publish_year}
+      subject={book.subject}
+      bookID={book.id}
+      bookStatus={formatStatus(book.status)}
+      setCurrBook={setCurrBook}
+    />
+  ));
+
   const parsedList =
-    books &&
+    results &&
     Object.values(results).map((book) => (
       <BookListItem
         title={book.title}
@@ -115,6 +82,9 @@ export default function BookList(props) {
       />
     ));
 
+  console.log('Book Count', initList.length);
+  const resultsCount = parsedList ? parsedList.length : results;
+  console.log('Search Results', resultsCount);
   return (
     <section>
       <Form className='search-books-bar'>
@@ -124,27 +94,13 @@ export default function BookList(props) {
           name='searchBook'
           type='text'
           className='search-bar'
-          value={form.value}
+          value={form}
           placeholder='Search by title, authour, genre etc.'
         />
-        <Button
-          variant='outline-primary'
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          Search
-        </Button>
-        <Button
-          variant='outline-dark'
-          onClick={() => {
-            clickReset();
-          }}
-        >
-          Reset
-        </Button>
       </Form>
-      <div className='cards'>{parsedList}</div>
+      <div className='cards'>
+        {resultsCount || form ? parsedList : initList}
+      </div>
     </section>
   );
 }
