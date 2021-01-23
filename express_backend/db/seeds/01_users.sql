978288d056533bc7cdff127247324d7060b999cd
dimontejulia <dimontejulia@gmail.com>
2021-01-23 18:46:01 -0500

    fixed seeds

--- express_backend/db/seeds/01_users.sql
+++ express_backend/db/seeds/01_users.sql
@@ -2,13 +2,13 @@ INSERT INTO
   users(first_name, last_name, email, password, age, gender, created_at)
 VALUES
 ('Mark', 'Twain', 'mark@books.com', 'password123', 76, 'M', '2020-01-02 10:19:15'),
-('Randi', 'Buzza', 'rbuzza1@hibu.com', 'password123', 23, 'M', '2020-03-29 18:35:14'),
-('Liuka', 'Bortolotti', 'lbortolotti2@stumbleupon.com', 'password123', 36, 'F', '2019-10-29 22:17:51'),
-('Kennett', 'Goldster', 'kgoldster3@xinhuanet.com', 'password123', 43, 'M', '2021-01-11 11:20:37'),
-('Addie', 'Creebo', 'acreebo4@tumblr.com', 'password123', 25, 'M', '2019-09-18 18:26:34'),
-('Arleyne', 'Dumini', 'adumini5@berkeley.edu', 'password123', 48, 'F', '2020-05-29 05:17:49'),
-('Gerardo', 'Shevlin', 'gshevlin6@seattletimes.com', 'password123', 35, 'M', '2020-08-15 09:36:03'),
-('Ignacio', 'Goldin', 'igoldin7@oakley.com', 'password123', 29, 'M', '2019-11-25 01:43:54'),
-('Emilia', 'O''Shevlan', 'eoshevlan8@reference.com', 'password123', 30, 'F', '2020-05-02 04:29:06'),
-('Kay', 'Skittles', 'kskittles9@illinois.edu', 'password123', 61, 'F', '2020-10-15 08:46:28')
+('Charles', 'Dickens', 'charles@books.com', 'password123', 23, 'M', '2020-03-29 18:35:14'),
+('Virginia', 'Woolf', 'virginia@books.com', 'password123', 36, 'F', '2019-10-29 22:17:51'),
+('Ernest', 'Hemingway', 'ernest@books.com', 'password123', 30, 'F', '2020-05-02 04:29:06'),
+('Maya', 'Angelou', 'maya@books.com', 'password123', 35, 'M', '2020-08-15 09:36:03'),
+('John', 'Steinbeck', 'john@books.com', 'password123', 43, 'M', '2021-01-11 11:20:37'),
+('Jane', 'Austen', 'jane@books.com', 'password123', 25, 'F', '2019-09-18 18:26:34'),
+('Oscar', 'Wilde', 'oscar@books.com', 'password123', 48, 'M', '2020-05-29 05:17:49'),
+('Lewis', 'Carroll', 'lewis@books.com', 'password123', 29, 'M', '2019-11-25 01:43:54'),
+('Roald', 'Dahl', 'roald@books.com', 'password123', 61, 'F', '2020-10-15 08:46:28')
 ;
