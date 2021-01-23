90c017b8a3cf4bee15fc8a83162b94afb33a78b4
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-22 22:01:00 -0500

    solved conlfict

--- client/src/components/Club/ClubNews.jsx
+++ client/src/components/Club/ClubNews.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ClubNewsForm from "../Social/NewPostForm";
-import ClubNewsFeed from "../Social/NewsFeed"; //newsList & Name
-import NewsItem from "../Social/NewsFeedItem";
-
-export default function ClubInfo(props) {
-  const { postClubNews, clubNews, clubId } = props;
-
-  //Iterate over News Obj for this Club's News
-  //Generate Items
-  clubNews &&
-    Object.values(clubNews).map((newsItem) => {
-      console.log("MAPS!!!", newsItem);
-    });
-
-  return (
-    <section>
-      <ClubNewsForm user={} clubInfo={} postClubNews={} />
-      <ClubNewsFeed newsList={clubNews} listName="Club News:" />
-=======
 import React from 'react';
 import ClubNewsForm from './ClubNewsForm';
 import ClubNewsFeed from '../Social/NewsFeed';
@@ -31,7 +10,6 @@ export default function ClubInfo(props) {
     <section>
       <ClubNewsForm postClubNews={postClubNews} />
       <ClubNewsFeed newsList={clubNews} listName='Club News:' />
->>>>>>> feature/clubNews
     </section>
   );
 }
