8755a66c0b423ca9e7fffd93c01a4fe80de245a4
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-23 14:02:50 -0500

    Club Info shows club Members & history

--- client/src/components/Club/ClubInfo.jsx
+++ client/src/components/Club/ClubInfo.jsx
@@ -4,14 +4,21 @@ import ClubRegular from './ClubRegular';
 import ClubAdmin from './ClubAdmin';
 import ClubNews from './ClubNews';
 import ClubHistory from './ClubHistory';
+import ClubMembers from '../List';
 
 export default function ClubInfo(props) {
   const bookDetails = props.currBook;
 
   //getting book history - prop manipulation
-  const numBooks = Object.keys(props.currClub).length - 10;
-  let bookHistory = [];
-  for (var i = 0; i < numBooks; i++) bookHistory[i] = props.currClub[i];
+  // const numBooks = Object.keys(props.currClub).length - 10;
+  // let bookHistory = [];
+  // for (var i = 0; i < numBooks; i++) bookHistory[i] = props.currClub[i];
+
+  const members = props.currClub.members
+    ? props.currClub.members.map((member) => Object.values(member))
+    : null;
+
+  const bookCount = props.currClub.history ? props.currClub.history.length : 0;
 
   return (
     <div>
@@ -41,7 +48,15 @@ export default function ClubInfo(props) {
         <h3>Current Book:</h3>
         <Details book={bookDetails ? bookDetails : null} />
       </section>
-      <ClubHistory bookHistory={bookHistory} numBooks={numBooks} />
+      {/* <ClubHistory
+        bookHistory={props.currClub.history}
+        numBooks={props.currClub.history ? props.currClub.history.length : 0}
+      /> */}
+      <ClubMembers
+        listName={`Book Club History : ${bookCount} books read`}
+        list={props.currClub.history}
+      />
+      <ClubMembers listName={'Members:'} list={members} />
       <ClubNews clubNews={props.clubNews} postClubNews={props.postClubNews} />
     </div>
   );
