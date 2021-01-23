b70648732168eb096da7458dfb136fde00a4a6c5
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-23 16:25:23 -0500

    Search Iterates over nested arrays

--- client/src/components/BookShelf/BookList.jsx
+++ client/src/components/BookShelf/BookList.jsx
@@ -1,51 +1,54 @@
-import React, { useState } from "react";
-import BookListItem from "./BookListItem";
-import CardDeck from "react-bootstrap/CardDeck";
-import FormControl from "react-bootstrap/FormControl";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect, useCallback } from 'react';
+import BookListItem from './BookListItem';
+import CardDeck from 'react-bootstrap/CardDeck';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-import "../Shelf.scss";
+import '../Shelf.scss';
 
 export default function BookList(props) {
   const { books, setUserBooks, setCurrBook } = props;
   const [form, setForm] = useState([]);
-  const [searchWord, setSearchWord] = useState("");
+  const [searchWord, setSearchWord] = useState('');
+
+  console.log('Form', form);
+  console.log('Word', props);
 
   const formatStatus = (inputStatus) => {
     switch (inputStatus) {
-      case "in_progress":
-        return "Reading";
+      case 'in_progress':
+        return 'Reading';
 
-      case "finished":
-        return "Read it";
+      case 'finished':
+        return 'Read it';
         break;
     }
   };
 
   const handleChange = (e) => {
     const { value } = e.target;
-    setForm((prevState) => ({
-      value,
-    }));
+    setForm(value);
   };
 
   const handleClick = (e) => {
     setSearchWord(form.value);
     //Form resets w/o message and regardless of success or error...
-    setForm((prev) => ({ ...prev, value: "" }));
+    setForm((prev) => ({ ...prev, value: '' }));
   };
   const clickReset = (e) => {
-    setSearchWord("");
-    setForm((prev) => ({ ...prev, value: "" }));
+    setSearchWord('');
+    setForm((prev) => ({ ...prev, value: '' }));
   };
 
   const booklistitem = function (book) {
+    let subject = book.subject;
+
     if (Array.isArray(book.subject)) {
-      book.subject = book.subject.join();
+      subject = book.subject.join();
     }
     if (
-      book.subject.toLowerCase().includes(searchWord.toLowerCase()) ||
+      subject.toLowerCase().includes(searchWord.toLowerCase()) ||
       book.title.toLowerCase().includes(searchWord.toLowerCase()) ||
       book.author.toLowerCase().includes(searchWord.toLowerCase())
     ) {
@@ -63,23 +66,56 @@ export default function BookList(props) {
     }
   };
 
+  //map- take in obj of objs (books)
+  const searchBooks = useCallback(
+    (bookObj, searchTerm) => {
+      if (Array.isArray(searchTerm)) {
+        searchTerm = searchTerm.join();
+      }
+      searchTerm = searchTerm.toLowerCase();
+      const results = Object.values(books).map((book) => {
+        console.log('SEARCH', book);
+        console.log('Term', searchTerm);
+
+        return Object.values(book).map((val) => {
+          // console.log('val', val);
+          return val.toString().includes(searchTerm);
+        });
+        // books.id
+      });
+    },
+    [form]
+  );
+  //iterate over books obj - values
+  //Iterate over values of inner obj
+  //Check values for search term (includes
+  //Feed new arry into parsing map
+
+  useEffect(() => {
+    //Run Search of myBooks Generate new List
+    console.log('USE EFFECT FORM');
+    searchBooks(books, form);
+  }, [form]);
+  //UseEffect watching searchTerm
+  //When term changes run
+
   const parsedList =
     books && Object.values(books).map((book) => booklistitem(book));
 
   return (
     <section>
-      <Form className="search-books-bar">
+      <Form className='search-books-bar'>
         <FormControl
           onChange={handleChange}
-          id="searchBook"
-          name="searchBook"
-          type="text"
-          className="search-bar"
+          id='searchBook'
+          name='searchBook'
+          type='text'
+          className='search-bar'
           value={form.value}
-          placeholder="Search by title, authour, genre etc."
+          placeholder='Search by title, authour, genre etc.'
         />
         <Button
-          variant="outline-primary"
+          variant='outline-primary'
           onClick={() => {
             handleClick();
           }}
@@ -87,7 +123,7 @@ export default function BookList(props) {
           Search
         </Button>
         <Button
-          variant="outline-dark"
+          variant='outline-dark'
           onClick={() => {
             clickReset();
           }}
@@ -95,7 +131,7 @@ export default function BookList(props) {
           Reset
         </Button>
       </Form>
-      <div className="cards">{parsedList}</div>
+      <div className='cards'>{parsedList}</div>
     </section>
   );
 }
