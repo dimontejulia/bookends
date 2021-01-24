f95b18c46b92bae6d9a6a5e62ac9279cfd1e5d3a
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-01-24 10:04:02 -0500

    Share btn

--- client/src/components/ShareBook.jsx
+++ client/src/components/ShareBook.jsx
@@ -27,19 +27,6 @@ export default function ShareBook(props) {
     </Popover>
   );
 
-  const accorion = (
-    <Accordion defaultActiveKey='0'>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='0'>
-          Click me!
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey='0'>
-          <Card.Body>Hello! I'm the body</Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
-  );
-
   //   <OverlayTrigger
   //   rootClose={true}
   //   trigger='click'
