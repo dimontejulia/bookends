84c492206bcd96f945bf6dc1b22c122496dd1b3a
dlinardi <dlinardi98@gmail.com>
2021-01-25 16:06:46 -0500

    changed accordian for popover/reformatted/styled

--- client/src/components/ShareBook.jsx
+++ client/src/components/ShareBook.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Accordion, Card } from "react-bootstrap";
 import NewPostForm from "./Social/NewPostForm";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+import Button from "react-bootstrap/Button";
 
 export default function ShareBook(props) {
-  return (
-    <Accordion>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="0">
-          Share Book
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey="0">
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">Share Book</Popover.Title>
+      <Popover.Content>
+        <div>
           <NewPostForm
             user={props.user}
             setNews={props.setNews}
@@ -18,8 +19,24 @@ export default function ShareBook(props) {
               body: "",
             }}
           />
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+        </div>
+        <Button block variant="light" onClick={() => document.body.click()}>
+          Cancel
+        </Button>
+      </Popover.Content>
+    </Popover>
+  );
+
+  return (
+    <OverlayTrigger
+      rootClose={true}
+      trigger="click"
+      placement="right"
+      overlay={popover}
+    >
+      <Button block variant="outline-primary">
+        Share Book
+      </Button>
+    </OverlayTrigger>
   );
 }
