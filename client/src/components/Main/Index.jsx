e880d9c56e24cc9f75159c8cd8dd2e9fb70f7d8c
dlinardi <dlinardi98@gmail.com>
2021-01-26 12:20:23 -0500

    svg cleanup

--- client/src/components/Main/Index.jsx
+++ client/src/components/Main/Index.jsx
@@ -74,7 +74,7 @@ export default function Index(props) {
           <g
             id="Page-1"
             stroke="none"
-            stroke-width="1"
+            strokeWidth="1"
             fill="none"
             fill-rule="evenodd"
           >
