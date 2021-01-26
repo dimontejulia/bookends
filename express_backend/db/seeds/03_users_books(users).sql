86a7f90d5eca70c5ef21407c0ac08e79a262e0c5
dimontejulia <dimontejulia@gmail.com>
2021-01-26 16:01:23 -0500

    user books cleanup

--- express_backend/db/seeds/03_users_books(users).sql
+++ express_backend/db/seeds/03_users_books(users).sql
@@ -5,7 +5,7 @@ VALUES
   (1, 'OL18290341M', '2011-09-01 15:30:26', 3, 'Had to read for highschool. Would like to re-visit this title as an adult.', 'Finished'),
   (1, 'OL28166116M', '2021-01-10 15:30:26', 0, '', 'In Progress'),
   (1, 'OL343856M', '2015-09-01 15:30:26', 3, 'I read very few biographies, so I have trouble evaluating this within its field. That said, I found it fascinating, but a bit drier than I typically like my (recreational) nonfiction.', 'Finished'),
-  (1, 'OL8870184M', '2021-01-09 15:30:26', 0, '', 'On my list'),
+  (1, 'OL8870184M', '2021-01-09 15:30:26', 0, '', 'In Progress'),
   (1, 'OL28419977M', '2015-06-06 15:30:26', 2, '', 'Finished'),
   --hunger games 
   (2, 'OL28419977M', '2019-09-01 15:30:26', 0, 'Just picked this up. Excited to start it!', 'New'),
--- express_backend/db/seeds/05_future_books.sql
+++ express_backend/db/seeds/05_future_books.sql
@@ -1,6 +1,5 @@
  insert into future_books (user_id, book_id) 
  values
   (1, 'OL7511248M'),
-  (1, 'OL27579080M'),
-  (1, 'OL28270888M'),
-  (1, 'OL28212280M')
+  (1, 'OL26790972M'),
+  (1, 'OL6492763M')
